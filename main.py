fb4e4a9b8ecf47905ea9f3bc9d8184f0e750002b
Lun <153135253+Lunw07@users.noreply.github.com>
2025-12-18 20:22:52 +0000

    Added next_turn function to handle player moves and update label

--- main.py
+++ main.py
@@ -2,22 +2,40 @@ from tkinter import *
 import random
 
 def next_turn(row, column):
+
+    global player
+    global turn_label
+
+    if buttons[row][column]["text"] == "":
+        buttons[row][column]["text"] = player
+        buttons[row][column]["font"] = ("Arial", 40, "bold")  # bigger font
+
+        buttons[row][column]["state"] = DISABLED
+
+        if player == players[0]:
+            player = players[1]
+        else:
+            player = players[0]
+
+        turn_label.config(text = player + "' s Turn", fg = players_colour[player])
+
     pass
 
+def winner():
 
-
-
-
-
+    pass
 
 
 window = Tk()          # instantiates an instance of window 
 
-window.geometry("500x550")
+window.geometry("600x700")
 window.title("TicTacToe")
 
 players = ["x", "o"]
 player = random.choice(players)
+
+players_colour = {players[0]: "red",
+                  players[1]: "blue"}
 
 logo = PhotoImage(file='tictactoe.png')
 window.iconphoto(True, logo)
@@ -27,16 +45,20 @@ buttons = [[0,0,0],
            [0,0,0],
            [0,0,0]]
 
-label = Label(window, text = player + "' s Turn", font = ("Arial", 15))
-label.pack()
+results = [["","",""],
+           ["","",""],
+           ["","",""]]
+
+turn_label = Label(window, text = player + "' s Turn", font = ("Arial", 15), fg = players_colour[player])
+turn_label.pack()
 
 frame = Frame(window)
 frame.pack()
 
-for row in range(3):
-    for column in range(3):
-        buttons[row][column] = Button(frame, text = "", width = 16, height = 8, 
-                                      command = lambda row=row, column = column: next_turn(row,column))
+for row in range(len(buttons)):
+    for column in range(len(buttons[0])):
+        buttons[row][column] = Button(frame, text="",font=('Arial',40), width=5, height=3,
+                                      command= lambda row=row, column=column: next_turn(row,column))
         buttons[row][column].grid(row=row, column=column)
 
 window.mainloop()
